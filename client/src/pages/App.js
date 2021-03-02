4be0b88b04f9db9bfdc86538f8dbdef35abddccc
trannolis <tranno0lis@gmail.com>
2021-03-01 22:13:43 -0500

    src folder changes

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,21 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          JFK Airport Traffic Management System
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/trannolis/JFK-Traffic-Management-System"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          View Repository
         </a>
       </header>
     </div>
